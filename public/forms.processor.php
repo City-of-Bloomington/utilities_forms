f647b17ec1215ab6895153b2959c48d4fff553f7
inghamn <inghamn@bloomington.in.gov>
2016-03-22 11:41:36 -0400

    Files are now transferred to OnBase using FTP over SSL
    Fixes #3

--- public/forms.processor.php
+++ public/forms.processor.php
@@ -45,7 +45,7 @@ class Forms {
     private $form_pdf_filename;
     private $form_pdf_path;
 
-    public static validExtensions = ['jpg', 'png', 'gif', 'tiff', 'pdf'];
+    public static $validExtensions = ['jpg', 'png', 'gif', 'tiff', 'pdf'];
 
 
 	//SETUP VARIABLES AND PATHS AND CALL FUNCTIONS DEPENDING ON THE REQUEST
@@ -77,7 +77,7 @@ class Forms {
 
 			//REMOVED SPECIAL READ AGREEMENT HTML CREATION PER JOSH ON 2016-02-15
 			if ($_POST['DocumentType'] !== "SPECIALREADAGREEMENT") {
-				$tmp = $this->Create_Form_HTML();
+				$this->Create_Form_HTML();
 			}
 			$this->Create_Form_DIP();
 		}
@@ -101,26 +101,26 @@ class Forms {
 		if (isset($this->vals['Conf_Number'])) {
 			//SUPPLEMENTARY IMAGE FILES AND DIP FILES
 			//supp DIP file name example: 20151204122913-000608-0382-supp.txt
-			$this->supp_dip_path = ONBASE_PATH . "/$date-{$this->vals['Conf_Number']}-supp.txt";
+			$this->supp_dip_path = TEMP_PATH . "/$date-{$this->vals['Conf_Number']}-supp.txt";
 
 			//supp file name example: 20151204122913-000608-0382-img3.jpg
-			$this->supp_image_file_name = "/$date-{$this->vals['Conf_Number']}-img";
-			$this->supp_image_temp_path = ONBASE_PATH;
+			$this->supp_image_file_name = "$date-{$this->vals['Conf_Number']}-img";
+			$this->supp_image_temp_path = TEMP_PATH;
 		}
 
 		//FORM DIP FILES
 		//form DIP file example:20151204122846-000608-0382-form.txt
-		$this->form_dip_path = ONBASE_PATH . "/$date-{$this->Conf_Number}-form.txt";
+		$this->form_dip_path = TEMP_PATH . "/$date-{$this->Conf_Number}-form.txt";
 
 		//FORM HTML FILES
 		//form HTML file example:20151204122846-000608-0382-html.html
 		$this->form_html_filename = "$date-{$this->Conf_Number}-html.html";
-		$this->form_html_path = ONBASE_PATH . "/{$this->form_html_filename}";
+		$this->form_html_path = TEMP_PATH . "/{$this->form_html_filename}";
 
 		//FORM PDF FILES
 		//form PDF file example:20151204122846-000608-0382-form.pdf
 		$this->form_pdf_filename = "$date-{$this->Conf_Number}-form.pdf";
-		$this->form_pdf_path = ONBASE_PATH . "/{$this->form_pdf_filename}";
+		$this->form_pdf_path = TEMP_PATH . "/{$this->form_pdf_filename}";
 	}
 
 	//VALIDATE FILE UPLOADS
@@ -146,10 +146,13 @@ class Forms {
             }
 
             if (empty($this->Files[$counter]['Response'])) {
-				$New_Name = $this->supp_image_file_name . $counter . "." . $extension;
-				if (move_uploaded_file($File["tmp_name"], $this->supp_image_temp_path . $New_Name)) {
+                $imageFile = "{$this->supp_image_temp_path}/{$this->supp_image_file_name}$counter.$extension";
+				if (move_uploaded_file($File["tmp_name"], $imageFile)) {
+                    self::transferFileToOnBase($imageFile);
+                    unlink($imageFile);
+
 					$this->Files[$counter]['Response'] = "Success";
-					$this->Files[$counter]['Filename'] = $New_Name;
+					$this->Files[$counter]['Filename'] = basename($imageFile);
 					$this->Valid_Counter++;
 				}
             }
@@ -166,21 +169,22 @@ class Forms {
 	//WRITE THE SUPPLEMENTARY DIP FILE
 	private function Create_Supp_DIP()
 	{
-		$line = "";
+		$data    = "";
 		$message = "";
 		foreach ($this->Files as $counter => $val) {
 			if ($val['Filename'] !== "") {
-				$line .= str_pad("DocumentTypeNum:",30," ",STR_PAD_RIGHT) . "340\r\n";
-				$line .= str_pad("Conf_Number:",30," ",STR_PAD_RIGHT) . $this->vals['Conf_Number'] . "\r\n";
-				$line .= str_pad("SupplementaryType:",30," ",STR_PAD_RIGHT) . $this->vals['Sup_Type_'.$counter] . "\r\n";
-				$line .= str_pad("Filename:",30," ",STR_PAD_RIGHT) . $val['Filename'] . "\r\n";
+				$data .= str_pad("DocumentTypeNum:",  30," ",STR_PAD_RIGHT) . "340\r\n";
+				$data .= str_pad("Conf_Number:",      30," ",STR_PAD_RIGHT) . $this->vals['Conf_Number'] . "\r\n";
+				$data .= str_pad("SupplementaryType:",30," ",STR_PAD_RIGHT) . $this->vals['Sup_Type_'.$counter] . "\r\n";
+				$data .= str_pad("Filename:",         30," ",STR_PAD_RIGHT) . $val['Filename'] . "\r\n";
 			}
 			$message .= "Message[" . $val['Old_Filename']. "]=" . $val['Response'] . "&";
 		}
-		$handle = fopen($this->supp_dip_path,"w");
-		fwrite($handle,$line);
-		fclose($handle);
-		if (file_exists($this->supp_dip_path)) {
+		file_put_contents($this->supp_dip_path, $data);
+		if (file_exists(  $this->supp_dip_path)) {
+            self::transferFileToOnBase($this->supp_dip_path);
+            unlink($this->supp_dip_path);
+
 			if ($this->vals['DocumentType'] === "SUPPLEMENTARYDOCUMENT") {
                 $url = BASE_URL."/index.php?form=SuppConfirmation&Confirmation_Number={$this->vals['Conf_Number']}&$message";
 				header("Location: $url");
@@ -213,7 +217,7 @@ class Forms {
 	private function Get_Mapped_Name($ob)
 	{
 		foreach ($this->xml->form as $form) {
-			if (strtoupper($form->file) === $this->vals['DocumentType']) {
+			if (strtoupper($form->file) == $this->vals['DocumentType']) {
 				$this->Docs = $form->docs;
 				foreach ($form->fields as $val) {
 					foreach ($val as $field) {
@@ -227,26 +231,11 @@ class Forms {
 		}
 	}
 
-	//THIS FUNCTION IS NOT USED, IT WAS A TEST TO GENERATE PDF
-	private function Create_Form_PDF($tmp)
-	{
-		$cmd = "wkhtmltopdf " . $tmp . " " . $this->form_pdf_path;
-		exec($cmd);
-
-		if (file_exists($this->form_pdf_path)) {
-			unlink($tmp);
-		}
-	}
-
 	private function Create_Form_HTML()
 	{
-		//GET THE FORM HTML AS THE CUSTOMER SEES IT (INCLUDING CSS AND JAVASCRIPT FILES) AND SAVE IT TO A TEMPORARY LOCATION
-		$tmp = APPLICATION_HOME . "/data/temp/" . $this->form_html_filename;
-		file_put_contents($tmp, file_get_contents(BASE_URL."/index.php?form=" . strtolower($_POST['DocumentType'] . "&html=true")));
-
-		$html = "";
-		$lines = file($tmp);
-		//START READING THE HTML FILE LINE BY LINE
+		//GET THE FORM HTML AS THE CUSTOMER SEES IT (INCLUDING CSS AND JAVASCRIPT FILES)
+		$url = BASE_URL.'/index.php?form=' . strtolower($_POST['DocumentType']) . '&html=true';
+		$lines = file($url);
 		foreach ($lines as $line_num => $line) {
 			if (strpos($line,"global.js")) {
 				$line = str_replace("global.js","onbase.js",$line);
@@ -304,13 +293,12 @@ class Forms {
 				}
 			}
 		}
-		//WRITE THE NEW HTML TO A FILE
+
 		file_put_contents($this->form_html_path,$html);
-		if (file_exists($this->form_html_path)) {
-			//IF THE NEW FILE EXISTS, DELETE THE TEMP FILE
-			unlink($tmp);
+		if (file_exists(  $this->form_html_path)) {
+			self::transferFileToOnBase($this->form_html_path);
+			unlink($this->form_html_path);
 		}
-		return $tmp;
 	}
 
 	//WRITE THE FORM DIP FILE
@@ -321,14 +309,16 @@ class Forms {
 		if ($_POST['DocumentType'] !== "SPECIALREADAGREEMENT") {
 			$this->vals['Filename'] = $this->form_html_filename;
 		}
-		$line = "";
+		$data = "";
 		foreach ($this->vals as $key => $val) {
-			$line .= str_pad($this->Get_Mapped_Name($key).":",30," ",STR_PAD_RIGHT) . $val . "\r\n";
+			$data .= str_pad($this->Get_Mapped_Name($key).":",30," ",STR_PAD_RIGHT) . $val . "\r\n";
 		}
-		$handle = fopen($this->form_dip_path,"w");
-		fwrite($handle,$line);
-		fclose($handle);
+
+		file_put_contents($this->form_dip_path, $data);
 		if (file_exists($this->form_dip_path)) {
+            self::transferFileToOnBase($this->form_dip_path);
+            unlink($this->form_dip_path);
+
 			$this->Docs = $this->Docs - $this->Valid_Counter;
             $url = BASE_URL."/index.php?form=Confirmation&Confirmation_Number={$this->Conf_Number}&docs={$this->Docs}";
             header("Location: $url");
@@ -354,5 +344,44 @@ class Forms {
 		else {
 			throw new \Exception('Missing Extension');
 		}
+	}
+
+	/**
+	 * Takes care of sending a file to OnBase.
+	 *
+	 * Throws an exception if there's an error.
+	 * You must provide the full path to the file on the web server's hard drive
+	 *
+	 * @param string $file The full path to the file
+	 * @throws \Exception
+	 */
+	private static function transferFileToOnBase($file)
+	{
+        if (is_file($file)) {
+            $filename = basename($file);
+            $stream   = fopen($file, 'r');
+            $url      = sprintf('ftps://%s/%s', ONBASE_HOST, ONBASE_PATH);
+            $options = [
+                CURLOPT_UPLOAD     => true,
+                CURLOPT_FTP_SSL    => CURLFTPSSL_ALL,
+                CURLOPT_FTPSSLAUTH => CURLFTPAUTH_TLS,
+                CURLOPT_INFILESIZE => filesize($file),
+                CURLOPT_URL        => "$url/$filename",
+                CURLOPT_INFILE     => $stream,
+                CURLOPT_CAINFO     => ONBASE_CERT,
+                CURLOPT_USERPWD    => ONBASE_USER.':'.ONBASE_PASS
+            ];
+
+            $curl = curl_init($url);
+            foreach ($options as $key=>$value) {
+                if (!curl_setopt($curl, $key, $value)) { throw new \Exception(curl_error($curl)); }
+            }
+
+            if (!curl_exec($curl)) { throw new \Exception(curl_error($curl)); }
+            fclose($stream);
+        }
+        else {
+            throw new \Exception('unknownFile');
+        }
 	}
 }
