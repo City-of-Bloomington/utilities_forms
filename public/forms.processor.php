92d241e4a4f3b26be5bc79ced62b29bfdf559696
inghamn <inghamn@bloomington.in.gov>
2016-03-23 15:18:33 -0400

    Fixed cleaning of streetNumber and streetDirection fields
    Fixes #15

--- public/forms.processor.php
+++ public/forms.processor.php
@@ -5,23 +5,23 @@ include '../configuration.inc';
 if (isset($_POST)) {
 	if (count($_POST) > 1 && isset($_POST['DocumentType'])) {
 		$_POST['dateTimeStamp'] = date("Y-m-d H:i:s",time());
+
 		foreach ($_POST as $key => $post) {
-			//REMOVED PER REQUEST FROM JOSH HOWE ON 2016-02-15
-			//if ($key == "OBKey__257_1" && $post != "") {
-				//if ($_POST['DocumentType'] != "SPECIALREADAGREEMENT") {
-				//	$_POST['OBKey__121_1'] = $_POST['OBKey__257_1'];
-				//	unset($_POST['OBKey__257_1']);
-				//}
-			//}
 			$_POST[$key] = strtoupper(strip_tags($post));
 		}
 
-		//ADDED PER REQUEST FROM JOSH HOWE ON 2016-02-15
-		if ($_POST['OBKey__225_1'] === "") { $_POST['OBKey__226_1'] = ""; }
-		if ($_POST['OBKey__225_2'] === "") { $_POST['OBKey__226_2'] = ""; }
-		if ($_POST['OBKey__225_3'] === "") { $_POST['OBKey__226_3'] = ""; }
-		if ($_POST['OBKey__225_4'] === "") { $_POST['OBKey__226_4'] = ""; }
-		if ($_POST['OBKey__225_5'] === "") { $_POST['OBKey__226_5'] = ""; }
+		$addressFields = [
+            'OBKey__225_1' => 'OBKey__226_1',
+            'OBKey__225_2' => 'OBKey__226_2',
+            'OBKey__225_3' => 'OBKey__226_3',
+            'OBKey__225_4' => 'OBKey__226_4',
+            'OBKey__225_5' => 'OBKey__226_5'
+		];
+		foreach ($addressFields as $streetNumber=>$streetDirection) {
+            if (empty($_POST[$streetNumber]) && isset($_POST[$streetDirection])) {
+                $_POST[$streetDirection] = '';
+            }
+		}
 
 		$Forms = new Forms($_POST);
 	}
